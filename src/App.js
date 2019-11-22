d35979cabf2e84224fd327e01a1abbeb008b58a8
Jerome Simeon <jeromesimeon@me.com>
2019-11-22 12:17:32 -0500

    Initial code
    Signed-off-by: Jerome Simeon <jeromesimeon@me.com>

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Supplementary material for PLDI'2020 Submission
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          DB Cert
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
