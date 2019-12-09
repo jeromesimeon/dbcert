f0f991dba8da71525f40956fb24b2716180b7035
Jerome Simeon <jeromesimeon@me.com>
2019-12-09 11:04:36 -0500

    typo
    Signed-off-by: Jerome Simeon <jeromesimeon@me.com>

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-    return (<footer><center><h3>Copyrighted Material, please do not redistribute</h3></center></footer>);
+    return (<footer><center><h3>Copyrighted material, please do not redistribute</h3></center></footer>);
 }
 
 export default App;
