39a44d60176516afe3cdd0f7bfab09b5b3013a12
Jerome Simeon <jeromesimeon@me.com>
2019-12-09 11:03:19 -0500

    Some initial code for DBCert
    Signed-off-by: Jerome Simeon <jeromesimeon@me.com>

--- src/App.js
+++ src/App.js
@@ -2,23 +2,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Supplementary material for PLDI'2020 Submission
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          DB Cert
-        </a>
-      </header>
-    </div>
-  );
+    return (<footer><center><h3>Copyrighted Material, please do not redistribute</h3></center></footer>);
 }
 
 export default App;
